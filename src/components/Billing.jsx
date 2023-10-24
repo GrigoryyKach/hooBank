c996a686efe6ce795853d923a5b0ecc71e3cdb6b
Grygoryy Kachurovskyy <kachurovskyy123@gmail.com>
2023-10-24 14:18:18 +0300

    billing section added

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,7 +10,33 @@ const Billing = () => (
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
     </div>
 
-    <div className={layout.sectionInfo}></div>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <a href="https://www.apple.com/ua/app-store/">
+          <img
+            src={apple}
+            alt="app_store"
+            className="w-[128px] h-[42px] object-contain mr-5"
+          />
+        </a>
+        <a href="https://play.google.com/store/games?hl=ru&gl=US&pli=1">
+          <img
+            src={google}
+            alt="google_play"
+            className="w-[128px] h-[42px] object-contain"
+          />
+        </a>
+      </div>
+    </div>
   </section>
 );
 
